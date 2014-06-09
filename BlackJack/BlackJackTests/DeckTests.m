6e6fa46fcf768b0ef0688420478cfa5f80c961bd
altyus <alctyus@gmail.com>
2014-06-09 16:31:13 -0400

    Added Pods

---
+++ BlackJack/BlackJackTests/DeckTests.m
@@ -0,0 +1,35 @@
+//
+//  DeckTests.m
+//  DeckTests
+//
+//  Created by Al Tyus on 6/9/14.
+//  Copyright (c) 2014 Flatiron School. All rights reserved.
+//
+
+#import "Specta.h"
+#define EXP_SHORTHAND
+#import <Expecta.h>
+
+
+SpecBegin(Deck)
+
+describe(@"Deck", ^{
+    
+    beforeAll(^{
+
+    });
+    
+    beforeEach(^{
+        
+    });
+    
+    describe(@"removeFriends", ^{
+        
+        beforeEach(^{
+        });
+        
+    });
+});
+
+
+SpecEnd
---
+++ BlackJack/Pods/Pods-BlackJackTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BlackJackTests : NSObject
+@end
+@implementation PodsDummy_Pods_BlackJackTests
+@end
