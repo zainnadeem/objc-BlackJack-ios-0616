df9915f74428d7465698786cad493c0eef4fdb58
markedwardmurray <markedwardmurray@gmail.com>
2015-10-05 21:11:17 -0400

    replace solution

--- BlackJack/FISAppDelegate.m
+++ BlackJack/FISAppDelegate.m
@@ -1,11 +1,23 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISBlackjackGame.h"
+#import "FISCard.h"
+#import "FISCardDeck.h"
 
 @implementation FISAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    FISBlackjackGame *game = [[FISBlackjackGame alloc] init];
+    
+    [game playBlackjack];
+
+    [game playBlackjack];
+    
+    [game playBlackjack];
+    
     return YES;
 }
 
